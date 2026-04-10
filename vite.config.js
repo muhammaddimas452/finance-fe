ef418de3fd9008b9bb7274bace18a66e5ab3ec26
Muhammad Dimas W <mdimaswakhid@gmail.com>
2026-04-10 21:28:39 +0700

    add base config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://github.com/muhammaddimas452/finance-fe",
+  base: "/finance-fe/",
   theme: {
     extend: {
       colors: {
